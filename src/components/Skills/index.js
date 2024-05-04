976378ea0f2945d9bfa4c22df598442a1618a36c
Fernando Divino de Moraes Júnior <kinggprobr@gmail.com>
2024-05-04 01:18:54 -0300

    arrumando os nomes da attribute SkillButton

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -14,16 +14,16 @@ function Skills() {
       <SkillButton skill="HTML" image="html"/>
       <SkillButton skill="CSS" image="css"/>
       <SkillButton skill="Javascript" image="javascript"/>
-      <SkillButton skill="NodeJS" image="NodeJS"/>
-      <SkillButton skill="Npm" image="NPM"/>
-      <SkillButton skill="ReactJS" image="React"/>
-      <SkillButton skill="MySQL" image="MySQL"/>
+      <SkillButton skill="NodeJS" image="nodejs"/>
+      <SkillButton skill="Npm" image="npm"/>
+      <SkillButton skill="ReactJS" image="react"/>
+      <SkillButton skill="MySQL" image="mysql"/>
       <SkillButton skill="PHP" image="php"/>
       <SkillButton skill="C Language" image="c"/>
-      <SkillButton skill="Python" image="Python"/>
-      <SkillButton skill="Notion" image="Notion"/>
+      <SkillButton skill="Python" image="python"/>
+      <SkillButton skill="Notion" image="notion"/>
       <SkillButton skill="GitHub" image="github"/>
-      <SkillButton skill="Vercel" image="Vercel"/>
+      <SkillButton skill="Vercel" image="vercel"/>
       <SkillButton skill="Visual Studio Code" image="vscode"/>
 
       </div>
